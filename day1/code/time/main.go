d7caca885c22428de0417250b42e5a888ebb542b
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-09-13 12:29:07 +0100

    improves day1 time example

--- day1/code/time/main.go
+++ day1/code/time/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
-	// BEGINC OMIT
+	// BEGIN OMIT
 	now := time.Now()
+	fmt.Printf("Now (Unix timestamp): %v\n", now.Unix())
 	fmt.Printf("Now (RFC 3339): %s\n", now.Format(time.RFC3339))
+	fmt.Printf("Now (custom): %s\n", now.Format("Jan _2 2006, 15:04"))
 
-	sometime, err := time.Parse(time.RFC3339, "2017-01-01T21:09:09+00:00")
+	sometime, err := time.Parse(time.RFC3339, "2017-04-01T14:17:09+00:00")
 	if err != nil {
 		log.Printf("Can't parse time: %v", err)
 	}
@@ -19,5 +21,5 @@ func main() {
 
 	diff := now.Sub(sometime)
 	fmt.Printf("Difference: %v or ca. %3.0f days\n", diff, diff.Hours()/24)
-	// ENDC OMIT
+	// END OMIT
 }
